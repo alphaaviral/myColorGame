2f2349193ecd77fa28faf07981d4060c3fb374c3
alphaaviral <alphaaviral@gmail.com>
2021-02-03 20:38:48 +0530

    added game over and lives

--- index.js
+++ index.js
@@ -1,46 +1,65 @@
-let t = [document.getElementById("t1"), document.getElementById("t2"), document.getElementById("t3"), document.getElementById("t4"), document.getElementById("t5"), document.getElementById("t6")];
-let x;
-let correctColor;
-let result = document.getElementById("result");
-let counter = document.getElementById("counter");
-let button = document.getElementById("refButton");
-let y;
-let z;
-function getRandomColor() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function newGame() {
-  y = 0;
-  counter.innerHTML = "Incorrect Guesses : " + y;
-  for(i=0; i<6; i++){
-      t[i].style.background = getRandomColor();
-  }
-
-  z = Math.floor(Math.random() * 6 + 1);
-
-  correctColor = t[z-1].style.background;
-  document.getElementById("header-clue").innerHTML = "Your color code is... " + correctColor;
-}
-
-for(let i=0; i<6; i++){
-t[i].addEventListener("click", function clickAction() {
-  if (t[i].style.background == correctColor) {
-    for(let b=0; b<6; b++){
-    t[b].style.background = correctColor;
+  let t = [document.getElementById("t1"), document.getElementById("t2"), document.getElementById("t3"), document.getElementById("t4"), document.getElementById("t5"), document.getElementById("t6")];
+  let x;
+  let correctColor;
+  let result = document.getElementById("result");
+  let counter = document.getElementById("counter");
+  let button = document.getElementById("refButton");
+  let y;
+  let z;
+  let r;
+  let m=0;
+  r = 6;
+  function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
-  } else {
-    t[i].style.background = getRandomColor();
-    y++
-    counter.innerHTML = "Incorrect Guesses : " + y;
-    x = Math.floor(Math.random() * 6 + 1);
-    correctColor = t[x-1].style.background;
-    document.getElementById("header-clue").innerHTML = "Your color code is... " + correctColor;
+    return color;
   }
-});
-}
+
+  function newGame() {
+    if(m == 1){
+      return;
+    }
+    y = 3;
+    counter.innerHTML = "Lives remaining : " + y;
+    for(i=0; i<6; i++){
+        t[i].style.background = getRandomColor();
+    }
+
+    z = Math.floor(Math.random() * 6 + 1);
+
+    correctColor = t[z-1].style.background;
+    document.getElementById("header-clue").innerHTML = "Your color code is... " + correctColor;
+
+  for(let i=0; i<6; i++){
+  t[i].addEventListener("click", function clickAction() {
+    if(m == 0){
+    if (t[i].style.background == correctColor) {
+      for(let b=0; b<6; b++){
+      t[b].style.background = correctColor;
+      }
+    } 
+    else {
+      t[i].style.background = getRandomColor();
+      y--;
+        counter.innerHTML = "Lives Remaining : " + y;
+        if(y ==0){
+          for(let p=0; p<6; p++){
+            t[p].style.background = "transparent";
+          }
+          document.getElementById("header-clue").innerHTML = "Game Over";
+          m=1;
+          newGame();
+        }
+      else {x = Math.floor(Math.random() * 6 + 1);
+      correctColor = t[x-1].style.background;
+      document.getElementById("header-clue").innerHTML = "Your color code is... " + correctColor;
+      }
+  }
+  }
+  })
+  }
+  }
+
