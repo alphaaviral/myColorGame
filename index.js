81c3ac00ece5029b366780cfe561ff9dbc24698c
alphaaviral <alphaaviral@gmail.com>
2021-02-03 21:49:07 +0530

    alerts added, point system added

--- index.js
+++ index.js
@@ -4,11 +4,12 @@
   let result = document.getElementById("result");
   let counter = document.getElementById("counter");
   let button = document.getElementById("refButton");
-  let y;
+  let y = 3;
   let z;
-  let r;
+  let r = 0;
   let m=0;
-  r = 6;
+  counter.innerHTML = "Lives Remaining : " + y;
+  document.getElementById("points").innerHTML = "Points : " + r;
   function getRandomColor() {
     let letters = "0123456789ABCDEF";
     let color = "#";
@@ -19,11 +20,6 @@
   }
 
   function newGame() {
-    if(m == 1){
-      return;
-    }
-    y = 3;
-    counter.innerHTML = "Lives remaining : " + y;
     for(i=0; i<6; i++){
         t[i].style.background = getRandomColor();
     }
@@ -32,34 +28,35 @@
 
     correctColor = t[z-1].style.background;
     document.getElementById("header-clue").innerHTML = "Your color code is... " + correctColor;
+  }
 
   for(let i=0; i<6; i++){
-  t[i].addEventListener("click", function clickAction() {
-    if(m == 0){
-    if (t[i].style.background == correctColor) {
-      for(let b=0; b<6; b++){
-      t[b].style.background = correctColor;
-      }
-    } 
-    else {
-      t[i].style.background = getRandomColor();
-      y--;
-        counter.innerHTML = "Lives Remaining : " + y;
-        if(y ==0){
-          for(let p=0; p<6; p++){
-            t[p].style.background = "transparent";
+    t[i].addEventListener("click", function clickAction() {
+      if(m == 0){
+      if (t[i].style.background == correctColor) {
+        newGame();
+        r++;
+        document.getElementById("points").innerHTML = "Points : " + r;
+        alert("Great!! You got it right.");
+      } 
+      else {
+        alert("Oops! You were wrong. You lost a life :(");
+        t[i].style.background = getRandomColor();
+        y--;
+          counter.innerHTML = "Lives Remaining : " + y;
+          if(y ==0){
+            for(let p=0; p<6; p++){
+              t[p].style.background = "transparent";
+            }
+            document.getElementById("header-clue").innerHTML = "Game Over";
+            m=1;
           }
-          document.getElementById("header-clue").innerHTML = "Game Over";
-          m=1;
-          newGame();
+        else {x = Math.floor(Math.random() * 6 + 1);
+        correctColor = t[x-1].style.background;
+        document.getElementById("header-clue").innerHTML = "Your color code is... " + correctColor;
         }
-      else {x = Math.floor(Math.random() * 6 + 1);
-      correctColor = t[x-1].style.background;
-      document.getElementById("header-clue").innerHTML = "Your color code is... " + correctColor;
-      }
-  }
-  }
-  })
-  }
-  }
-
+    }
+    }
+    })
+    }
+    console.log(correctColor);
